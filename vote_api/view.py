178bfc435048e8be46531e307f2a7fba20a4dd93
Larissa Gremelmaier Rosa <larissagr101@gmail.com>
2022-11-29 18:41:36 -0300

    Create vote API and initial database

---
+++ vote_api/view.py
@@ -0,0 +1,68 @@
+from typing import List, Dict
+from flask import Flask, request, jsonify
+import mysql.connector
+import json
+import time
+
+config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'vote_db'
+    }
+   
+# TODO: Arrumar para ter apenas uma conexão com db sempre ativa
+
+def search_elections() -> List[Dict]:
+    
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    time_now = time.time()
+    cursor.execute(f'SELECT * FROM Election WHERE timestampEnd >= {time_now}')
+    election_list  = cursor.fetchall()
+    results = [{"id":x[0],"name":x[1], "description":x[2]} for x in election_list]
+    cursor.close()
+    connection.close()
+    return results
+
+def get_election_options(content) -> List[Dict]:
+    
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    election = content['idElection']
+    cursor.execute(f'SELECT * FROM VoteOption WHERE idElection = {election}')
+    option_list  = cursor.fetchall()
+    results = [{"id":x[0],"name":x[1], "description":x[3]} for x in option_list]
+    cursor.close()
+    connection.close()
+    return results
+
+def create_election(content):
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    
+    val = (content['name'], content['timeEnd'], content['description'])
+    sql = '''INSERT INTO Election(name, timestampEnd, description) VALUES (\"%s\", %d, \"%s\")''' % val
+    try:
+        cursor.execute(sql)
+
+        if cursor.lastrowid:
+            election = cursor.lastrowid
+            print('last insert id', cursor.lastrowid)
+            for i in content['options']:
+                v = (i['name'], i['description'], election)
+                s = '''INSERT INTO VoteOption(name, description, idElection) VALUES (\"%s\", \"%s\", %d)''' % v
+                cursor.execute(s)
+        else:
+            print('last insert id not found')
+
+        connection.commit()
+    except Error as error:
+        print(error)
+        return error, 500
+
+    finally:
+        cursor.close()
+        connection.close()
+        return '', 204
---
+++ vote_db/init.sql
@@ -0,0 +1,29 @@
+create database if not exists vote_db;
+use vote_db;
+
+CREATE TABLE if not EXISTS Election(
+id INT(10) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100),
+timestampEnd INT(30),
+description VARCHAR(500)
+);
+
+CREATE TABLE if not EXISTS VoteOption(
+id INT(10) PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100),
+description VARCHAR(500),
+idElection INT(10),
+FOREIGN KEY (idElection) REFERENCES Election(id)
+);
+
+
+INSERT INTO Election(name, timestampEnd, description) VALUES ("Melhor filme", 1670696286, "Vote no seu filme favorito entre as opções de voto");
+INSERT INTO Election(name, timestampEnd, description) VALUES ("Cor mais amada", 1670696286, "Conte para gente qual cor você gosta mais");
+
+INSERT INTO VoteOption(name, description, idElection) VALUES ("Harry Potter e a camara secreta", "Segundo filme da saga Harry Potter", 1);
+INSERT INTO VoteOption(name, description, idElection) VALUES ("O iluminado", "Filme de terror", 1);
+INSERT INTO VoteOption(name, description, idElection) VALUES ("Enola Holmes", "Filme sobre a irmã do Sherlock", 1);
+
+INSERT INTO VoteOption(name, description, idElection) VALUES ("Azul", "A cor do céu", 2);
+INSERT INTO VoteOption(name, description, idElection) VALUES ("Vermelho", "A cor do sangue", 2);
+INSERT INTO VoteOption(name, description, idElection) VALUES ("Amarelo", "A cor do girassol", 2);
